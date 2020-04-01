a2486557074746196fadcfb81435811ec332fcc6
Umit <umitduran35@gmail.com>
2020-04-01 21:51:37 +0300

    login screen is designed

--- src/app.js
+++ src/app.js
@@ -2,40 +2,40 @@ import React, {Component} from 'react';
 import {StyleSheet, Image} from 'react-native';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {faKey} from '@fortawesome/free-solid-svg-icons';
-import {loginImg, backgroundImg} from './image';
+import {backgroundImg, signUpImg} from './image';
 import {
   Container,
-  Header,
   Content,
   Form,
   Item,
   Input,
-  Body,
   Title,
   Button,
   Label,
   Text,
+  View,
 } from 'native-base';
-import {red} from 'color-name';
 
 const styles = StyleSheet.create({
   container: {},
   loginButton: {
-    backgroundColor: '#FFE4B5',
-    marginTop: 20,
+    position: 'relative',
+    margin: 25,
+    backgroundColor: '#FF3366',
+    borderRadius: 36,
   },
   loginButtonText: {
     fontSize: 20,
+    color: 'white',
     fontStyle: 'normal',
   },
   formContent: {
-    padding: 30,
-    marginTop: 60,
+    padding: 20,
   },
   loginImg: {
     position: 'absolute',
-    left: 180,
-    bottom: 230,
+    left: 160,
+    bottom: 200,
     width: 75,
     height: 75,
     borderColor: 'green',
@@ -46,37 +46,71 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '240%',
   },
+  loginTitle: {
+    marginTop: 20,
+    fontFamily: 'Monoton-Regular',
+    fontSize: 80,
+    color: '#FF3366',
+  },
+  label: {
+    alignItems: 'center',
+    textAlign: 'center',
+    color: '#28848e',
+  },
 });
+const SignInLink = function() {
+  const signStyle = StyleSheet.create({
+    text: {
+      textAlign: 'center',
+      fontSize: 20,
+    },
+    signUpLink: {
+      textAlign: 'center',
+      fontSize: 25,
+      color: 'red',
+      marginRight: 2,
+    },
+    signUpIcon: {
+      width: 50,
+      height: 50,
+    },
+  });
+  return (
+    <View>
+      <Text style={signStyle.text}>You don't have an account !</Text>
+      <Text style={signStyle.signUpLink} onPress={onSignUp}>
+        Sign up <Image source={signUpImg} style={signStyle.signUpIcon} />
+      </Text>
+    </View>
+  );
+};
 
-export default class App extends Component {
-  render() {
-    return (
-      <Container style={styles.container}>
-        <Header>
-          <Body>
-            <Title>Login</Title>
-          </Body>
-        </Header>
-        <Content>
-          <Image source={backgroundImg} style={styles.loginBackground} />
-          <Form style={styles.formContent}>
-            <Image style={styles.loginImg} source={loginImg} />
+function onSignUp(e) {
+  //debugger;
+}
 
-            <Item floatingLabel>
-              <Label style={styles.textContent}>Username</Label>
-              <Input />
-            </Item>
+export default function App() {
+  return (
+    <Container style={styles.container}>
+      <Content>
+        <Image source={backgroundImg} style={styles.loginBackground} />
+        <Title style={styles.loginTitle}>iKra</Title>
+        <Form style={styles.formContent}>
+          <Item floatingLabel>
+            <Label style={styles.label}>Username</Label>
+            <Input />
+          </Item>
 
-            <Item floatingLabel last>
-              <Label style={styles.textContent}>Password</Label>
-              <Input />
-            </Item>
-            <Button transparent block style={styles.loginButton}>
-              <Text style={styles.loginButtonText}>SIGN IN</Text>
-            </Button>
-          </Form>
-        </Content>
-      </Container>
-    );
-  }
+          <Item floatingLabel last>
+            <Label style={styles.label}>Password</Label>
+            <Input />
+          </Item>
+          <Button transparent block style={styles.loginButton}>
+            <Text style={styles.loginButtonText}>Log In</Text>
+          </Button>
+          <SignInLink />
+        </Form>
+      </Content>
+    </Container>
+  );
 }
--- src/image/index.js
+++ src/image/index.js
@@ -1,2 +1,2 @@
-export const loginImg = require('./login.png');
 export const backgroundImg = require('./background.jpeg');
+export const signUpImg = require('./signup.png');
